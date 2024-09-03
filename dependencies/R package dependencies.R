43f8d8ca471507e215df50c1350daafab1a5f41c
rq1900 <77160012+rq1900@users.noreply.github.com>
2024-09-03 16:55:07 -0400

    Update R package dependencies.R

--- dependencies/R package dependencies.R
+++ dependencies/R package dependencies.R
@@ -1,3 +1,6 @@
+R requirement:
+version=4.1.1
+
 R packages requirements:
 SingleR
 celldex
@@ -5,16 +8,3 @@ SingleCellExperiment
 Seurat (version=4.3.0)
 anndata
 reticulate
-
-#------------------------------------------------------------------------------------
-#Demo code of installation:
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("SingleR")
-BiocManager::install("celldex")
-BiocManager::install("SingleCellExperiment") 
-
-install.packages("Seurat", version = '4.3.0')
-install.packages("anndata")
-install.packages("reticulate")
