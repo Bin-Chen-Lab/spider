ce15d29c76b29d430b4bb2fed45fea4c28d03da1
rq1900 <77160012+rq1900@users.noreply.github.com>
2024-09-03 16:49:33 -0400

    Update R package dependencies.R

--- dependencies/R package dependencies.R
+++ dependencies/R package dependencies.R
@@ -1,3 +1,13 @@
+R packages requirements:
+SingleR
+celldex
+SingleCellExperiment
+Seurat (version=4.3.0)
+anndata
+reticulate
+
+#------------------------------------------------------------------------------------
+#Demo code of installation:
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
