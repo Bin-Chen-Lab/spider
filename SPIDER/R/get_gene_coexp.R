174320ed5b028120e710d83868aed2d89e44eb7b
rq1900 <77160012+rq1900@users.noreply.github.com>
2024-09-03 03:10:46 -0400

    Update get_gene_coexp.R

--- SPIDER/R/get_gene_coexp.R
+++ SPIDER/R/get_gene_coexp.R
@@ -12,7 +12,9 @@ get_gene_coexp <- function(seurat_data, save_path, use_python_path){
   DefaultAssay(seurat_data) = "RNA"
   seurat_data <- NormalizeData(seurat_data, normalization.method = "LogNormalize", scale.factor = 10000)
   gene_coexp <- pd$DataFrame(t(as.matrix(seurat_data@assays$RNA@data)))$corr(method='pearson')
-  write.csv(gene_coexp, paste0(save_path, 'query_gene_coexpression.csv'))
+  gene_coexp$index = rownames(seurat_data)
+  gene_coexp$columns = rownames(seurat_data)
+  gene_coexp$to_csv(paste0(save_path, 'query_gene_coexpression.csv'))
   
   return(gene_coexp)
 }
